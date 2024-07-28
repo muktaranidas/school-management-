6e740c5afd646738c3756e34e0d48ed9434b6a38
mukta35-2733@diu.edu.bd <mukta35-2733@diu.edu.bd>
2024-07-28 10:38:55 +0600

    first commit

---
+++ app/components/Navbar/Navbar.js
@@ -0,0 +1,33 @@
+export default function Navbar() {
+  return (
+    <div className="navbar bg-[#192F59]">
+  <div className="flex-1">
+    <a className="btn btn-ghost ">daypicker use here</a>
+  </div>
+  <div className="flex-none">
+    <ul className="menu menu-horizontal px-1">
+      <li className="bg-[#FFFEF3]">
+        <details>
+          <summary>Login</summary>
+          <ul className="bg-base-100 rounded-t-none p-2">
+            <li><a>Bangla</a></li>
+            <li><a>hhdhdhh</a></li>
+          </ul>
+        </details>
+      </li>
+      <li className="bg-[#3DB166] text-[#FFFEF3]"><a>Bangla</a></li>
+    </ul>
+  </div>
+</div>
+  );
+}
+
+
+
+
+// export default function Navbar() {
+//   return (
+//     <div>
+//     </div>
+//   );
+// }
