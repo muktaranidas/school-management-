ab6fb1c08392e75f0d7214b49d03edcd4d06675d
mukta35-2733@diu.edu.bd <mukta35-2733@diu.edu.bd>
2024-07-29 13:16:25 +0600

    message

--- app/components/Message/Message.js
+++ app/components/Message/Message.js
@@ -1,19 +1,45 @@
 import Image from "next/image";
 export default function Message() {
   return (
-   <div className="grid grid-cols-3 mx-auto my-8">
-     <div className="card bg-base-100 w-96 shadow-xl">
-     <h2 className="card-title items-center justify-center text-center">Message Of The Principle</h2>   
-  <figure className="px-10 pt-10">
-  <Image src="/messageImage.png" width={210.26} height={193.87}/>
-  </figure>
-  <div className=" card-body items-center text-center">
-    <h2 className="card-title">Name</h2>
-    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+   <div className="grid grid-cols-3 mx-auto items-center justify-center my-8">
+        <div className="">
+            <h1 className="my-6 text-center text-2xl	font-bold	">Message of Chairman</h1>
+            <Image src="/messageImage.png"  width={210.26} height={193.87} className="mx-auto"/>
+            <div className="mt-6 mb-2">
+              <h2 className="text-xl font-semibold tracking-wide text-center">Name</h2>
+            </div>
+            <p className="text-base px-8">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+            </p>
+            {/* <button className=" mt-2 text-[#CA3214] text-base" >...Read More</button> */}
+        </div>
+        <div className="">
+            <h1 className="my-6 text-center text-2xl	font-bold	">Message of Chairman</h1>
+            <Image src="/messageImage.png"  width={210.26} height={193.87} className="mx-auto"/>
+            <div className="mt-6 mb-2">
+              <h2 className="text-xl font-semibold tracking-wide text-center">Name</h2>
+            </div>
+            <p className="text-base px-8">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+            </p>
+            {/* <button className=" mt-2 text-[#CA3214] text-base" >...Read More</button> */}
+        </div>
+        <div className="">
+            <h1 className="my-6 text-center text-2xl	font-bold	">Message of Chairman</h1>
+            <Image src="/messageImage.png"  width={210.26} height={193.87} className="mx-auto"/>
+            <div className="mt-6 mb-2">
+              <h2 className="text-xl font-semibold tracking-wide text-center">Name</h2>
+            </div>
+            <p className="text-base px-8">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+            </p>
+            {/* <button className=" mt-2 text-[#CA3214] text-base" >...Read More</button> */}
+        </div>
+        
+       
 
-  </div>
-  <p className="">...Read More</p>
-</div>
+
+   
    </div>
   );
 }
